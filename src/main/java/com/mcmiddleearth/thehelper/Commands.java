7e23e42de0ced9377a5a0b3b65e8b80bb9d028f5
Donovan S. Allen <dono.allen@gmail.com>
2014-06-02 17:25:35 -0600

    dev info password and ts stuff

--- src/main/java/com/mcmiddleearth/thehelper/Commands.java
+++ src/main/java/com/mcmiddleearth/thehelper/Commands.java
@@ -18,6 +18,9 @@ public class Commands implements CommandExecutor{
 //                   if(args[0].equalsIgnoreCase("converse")){
 //                       
 //                   }
+                   if(args[0].equalsIgnoreCase("teamspeak")||args[0].equalsIgnoreCase("ts")){
+                       player.sendMessage("TeamSpeak is how mcme communicates on projects. Download TeamSpeak 3 here: http://www.teamspeak.com/?page=downloads");
+                   }
                    if(args[0].equalsIgnoreCase("forums")){
                        player.sendMessage("The forums are where discussions take place and where you can find help. Visit: http://www.mcmiddleearth.com/forums/");
                    }
@@ -30,10 +33,15 @@ public class Commands implements CommandExecutor{
                }
            }
            if(command.getName().equalsIgnoreCase("devinfo")){
-               if(args.length > 0){
-                   if(args[0].equalsIgnoreCase("versions")){
-                       player.sendMessage("http://mcme.joshr.hk/server/build");
-                   }
+                char passchrs[] = {(char)109, (char)99, (char)109, (char)101, (char)100, (char)101, (char)118};
+                String pass = String.valueOf(passchrs);
+                char locchrs[] = {104, 116, 116, 112, 58, 47, 47, 109, 99, 109, 101, 046, 106, 111, 115, 104, 114, 046, 104, 107, 047, 115, 101, 114, 118, 101, 114, 047, 98, 117, 105, 108, 100};
+                String loc = String.valueOf(locchrs);
+                //I was really bored so I did this =)
+                if(args.length > 1){
+                    if(args[0].equalsIgnoreCase("versions") && args[1].equalsIgnoreCase(pass)){
+                        player.sendMessage(loc);
+                    }
                }
            }
         } else {
