0512d52c401544ae50eb0531eeed227609feffb2
= <me@donovanallen.net>
2018-07-07 12:28:42 -0600

    Rebuild and version bump

--- src/main/java/com/mcmiddleearth/thehelper/TheHelper.java
+++ src/main/java/com/mcmiddleearth/thehelper/TheHelper.java
@@ -1,41 +1,133 @@
 package com.mcmiddleearth.thehelper;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionDefault;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TheHelper extends JavaPlugin {
     
-    public static TheHelper pluginInstance;
+    final static HashMap<String, String> commands = new HashMap<>();
     
-    public HashMap<String, String> urls = new HashMap<>();
-    
-    public static ChatColor THccm = ChatColor.GREEN;
-    public static ChatColor THccl = ChatColor.RED;
-    
+    final static ChatColor THccm = ChatColor.GREEN;
+
+    final static Permission admin = new Permission("thehelper.admin", PermissionDefault.OP);
+
     @Override
     public void onEnable(){
-        Servlet server = new Servlet(8800);
-        server.start();
         this.saveDefaultConfig();
-        FileConfiguration config = this.getConfig();
-        pluginInstance=this;
-        getCommand("helper").setExecutor(new Commands());
-	getCommand("devinfo").setExecutor(new Commands());
-        if(this.getConfig().getBoolean("welcomeMsg")){
-            PluginManager pm = this.getServer().getPluginManager();
-            pm.registerEvents(new FirstJoinListener(), this);
+
+        for(String key : this.getConfig().getKeys(false)) {
+            commands.put(key, this.getConfig().getString(key));
         }
-        for(String s : config.getKeys(true)){
-            if(s.contains("url.")){
-                urls.put(s.replace("url.", ""), config.getString(s));
+
+        getCommand("helper").setExecutor(new Commands());
+    }
+
+    class Commands implements TabExecutor {
+        private void sendCommandHelp(CommandSender sender) {
+            sender.sendMessage("Valid help options:");
+            for(String key :  commands.keySet()) {
+                sender.sendMessage(" - /helper " + key);
+            }
+            if(sender.hasPermission(admin)) {
+                sender.sendMessage("/helper add <key> <value>");
+                sender.sendMessage("/helper remove <key>");
             }
         }
-    }
-    @Override
-    public void onDisable(){
-        
+
+        @Override
+        public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+            if(args.length < 1) {
+                sendCommandHelp(sender);
+                return true;
+            }
+
+            if(sender.hasPermission(admin) && args[0].equalsIgnoreCase("add")) {
+                if(args.length < 3) {
+                    sendCommandHelp(sender);
+                    return true;
+                }
+                StringBuilder messageBuilder = new StringBuilder();
+                for(int i = 2; i < args.length; i++) {
+                    messageBuilder.append(args[i]);
+                    if(i < args.length - 1) {
+                        messageBuilder.append(" ");
+                    }
+                }
+                commands.put(args[1], messageBuilder.toString());
+                if(TheHelper.this.getConfig().contains(args[1])) {
+                    TheHelper.this.getConfig().set(args[1], messageBuilder.toString());
+                    TheHelper.this.saveConfig();
+                } else {
+                    TheHelper.this.getConfig().addDefault(args[1], messageBuilder.toString());
+                    TheHelper.this.saveConfig();
+                }
+                sender.sendMessage("Help option added");
+                return true;
+            }
+            if(sender.hasPermission(admin) && args[0].equalsIgnoreCase("remove")) {
+                if(args.length < 2) {
+                    sendCommandHelp(sender);
+                    return true;
+                }
+                String message = commands.remove(args[1]);
+                TheHelper.this.getConfig().set(args[1], null);
+                TheHelper.this.saveConfig();
+                sender.sendMessage(message == null ? "Could not locate help option" : "Help option removed");
+                return true;
+            }
+
+            String help = commands.get(args[0]);
+            if(help == null) {
+                sendCommandHelp(sender);
+                return true;
+            }
+            sender.sendMessage(THccm + help);
+            return true;
+        }
+
+        @Override
+        public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+            if(args.length <= 1) {
+                ArrayList<String> possible = new ArrayList<>(commands.keySet());
+                if(sender.hasPermission(admin)){
+                    possible.add("add");
+                    possible.add("remove");
+                }
+                if(args.length == 0) {
+                    return possible;
+                }
+                ArrayList<String> actual = new ArrayList<>();
+                for(String k : possible) {
+                    if(k.startsWith(args[0])) {
+                        actual.add(k);
+                    }
+                }
+                return actual;
+            } else {
+                if(args[0].equalsIgnoreCase("add")) {
+                    return new ArrayList<>();
+                } else if(args[0].equalsIgnoreCase("remove")){
+                    ArrayList<String> possible = new ArrayList<>();
+                    for(String k : commands.keySet()) {
+                        if(k.startsWith(args[1])){
+                            possible.add(k);
+                        }
+                    }
+                    return possible;
+                }
+            }
+            return new ArrayList<>();
+        }
     }
 }
